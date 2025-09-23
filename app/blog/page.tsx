d06484d68a4c0b0d1edb29f9bba9f946070e57d9
Magiri1115 <busmagiri6@gmail.com>
2025-09-23 22:20:04 +0900

    blog完成

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,45 +1,42 @@
+"use client";
+
 export default function BlogPage() {
-  const contacts = [
+  const blogLinks = [
     {
-      label: "X (Twitter)",
-      href: "https://x.com/Magiri2001",
-      icon: "/icons/x.svg",
+      title: "グローバル企業のWebCSS構成から企業の意図を読み解く",
+      href: "https://docs.google.com/presentation/d/1w4zWHw_wn0hnZ22NFBshwq9akMWkpq05Y_phKca81HU/edit?slide=id.p#slide=id.p",
+      comment: "Webデザインを通じて企業メッセージを読み解く講義資料",
     },
     {
-      label: "Email",
-      href: "mailto:busmagiri6@example.com",
-      icon: "/icons/gmail.svg",
+      title: "Godotプログラミングの記述法",
+      href: "https://docs.google.com/presentation/d/1frJroJ6NNjcG8p7zmYtyYd-O7ZeHoQxe/edit?slide=id.p1#slide=id.p1",
+      comment: "軽量で迅速なゲーム開発を可能にしたGodotエンジンと記述法の講義資料",
     },
     {
-      label: "GitHub",
-      href: "https://github.com/Magiri1115",
-      icon: "/icons/github.svg",
-    },
-    {
-      label: "Discord",
-      icon: "/icons/discord.svg",
+      title: "ポートフォリオ",
+      href: "http://magiri.jp",
+      comment: "個人ポートフォリオ（このサイト）",
     },
   ];
 
   return (
-    <div className="text-center space-y-6">
-      <h1 className="text-3xl font-bold mb-6">Blog</h1>
-      <ul className="space-y-4">
-        {contacts.map((item) => (
-          <li key={item.label}>
+    <div className="min-h-screen flex flex-col items-center justify-center px-6 py-12 space-y-8 text-center">
+      <h1 className="text-4xl font-bold text-white">Blog</h1>
+      <ul className="space-y-10">
+        {blogLinks.map((item) => (
+          <li key={item.title} className="relative group">
             <a
               href={item.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center justify-center space-x-3 text-xl text-blue-500 hover:underline"
+              className="text-xl text-blue-400 hover:underline"
             >
-              <img
-                src={item.icon}
-                alt={item.label}
-                className="w-7 h-7 inline-block"
-              />
-              <span>{item.label}</span>
+              {item.title}
             </a>
+            {/* コメント要素（hover時に表示、1行固定） */}
+            <div className="absolute left-1/2 -translate-x-1/2 mt-2 whitespace-nowrap bg-black/80 text-white text-sm rounded-lg px-3 py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              {item.comment}
+            </div>
           </li>
         ))}
       </ul>
